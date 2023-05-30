b7e069c4c2d4d6adf3a94cab8b83b108ea52ad70
immanuel <immanuelajimah@gmail.com>
2023-05-30 23:05:24 +0100

    navBar component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div>about</div>
+      <div>form</div>
+      <div>footer</div>
+    </>
+  )
+}
+
+export default App
