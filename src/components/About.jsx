e7368eda8e0e72a4a24d8cbd02d93a0bcef83f75
immanuel <immanuelajimah@gmail.com>
2023-05-31 10:00:17 +0100

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,73 @@
-import { Box, Stack, Typography } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+} from '@mui/material'
 import './about.css'
 function About() {
   return (
     <Stack component="div" className="about">
-      <Box>
-        <Typography
-          sx={{ color: '#01796F', fontSize: '36px', lineHeight: '49px' }}
-        >
-          About Foodify <span>&#129312;</span>
-        </Typography>
-        <Typography variant="subtitle1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae,
-          commodi aliquam. Officia quidem corporis, obcaecati explicabo odio
-          minima blanditiis quia suscipit eius non totam, repudiandae aut harum
-          voluptas. Ratione perferendis accusamus quidem impedit eius libero
-          ipsa reprehenderit veniam totam consectetur qui dolores quaerat sunt
-          quam vero corrupti, molestias, nostrum eligendi neque atque est
-          facere. Quae iste velit vero. Voluptatum, alias?
-        </Typography>
-      </Box>
+      <div className="aboutContent">
+        <Box className="mainText">
+          <Typography
+            sx={{ color: '#01796F', lineHeight: '49px' }}
+            mb={5}
+            variant="h3"
+          >
+            About Foodify <span>&#129312;</span>
+          </Typography>
+          <Typography variant="subtitle1" mb={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repudiandae, commodi aliquam. Officia quidem corporis, obcaecati
+            explicabo odio minima blanditiis quia suscipit eius non totam, s
+            libero ipsa reprehenderit veniam totam consectetur qui uptatum
+          </Typography>
+          <Typography>
+            dolores quaerat sunt quam vero corrupti, molestias, nostrum eligendi
+            neque atque est facere. Quae iste velit vero. Voluptatum, alias? \
+            est facere. Quae iste velit vero. Volalias?
+          </Typography>
+        </Box>
 
-      <Box></Box>
+        <div className="card">
+          <Card className="cardItem">
+            <CardMedia></CardMedia>
+            <CardContent>
+              <Typography variant="h5">Ease of Use</Typography>
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                mollitia pariatur doloremque ipsum dolorum ducimus laudantium
+                alias quos distinctio veniam. Lorem ipsum dolor sit amet
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card className="cardItem">
+            <CardMedia></CardMedia>
+            <CardContent>
+              <Typography variant="h5">Cheapest Price</Typography>
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                mollitia pariatur doloremque ipsum dolorum ducimus laudantium
+                alias quos distinctio veniam. Lorem ipsum dolor sit amet
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card className="cardItem">
+            <CardMedia></CardMedia>
+            <CardContent>
+              <Typography variant="h5">Different Payment Plan</Typography>
+              <Typography>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                mollitia pariatur doloremque ipsum dolorum ducimus laudantium
+                alias quos distinctio veniam. Lorem ipsum dolor sit amet
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </Stack>
   )
 }
